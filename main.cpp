2d50f457cfea6cfd4364fee227249dfd5596e288
Vinayak Singla <singla.1@iitj.ac.in>
2018-01-05 20:31:28 +0530

    initial screen is now full size

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include "editor.h"
-
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[]) {
     
   QApplication app(argc, argv);  
-    
-  textEditor window;
 
-  window.resize(700, 500);
+  QRect screen = QApplication::desktop()->availableGeometry();
+  int height = screen.height();
+  int width = screen.width();
+  textEditor window;
+  window.resize(width, height);
   window.setWindowTitle("Personal Editor");
   window.show();
 
