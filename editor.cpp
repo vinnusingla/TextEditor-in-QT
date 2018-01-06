e2b2191af738bbd67aee82d6c6f441d23415d494
Vinayak Singla <singla.1@iitj.ac.in>
2018-01-06 10:05:01 +0530

    demo save function added

--- editor.cpp
+++ editor.cpp
@@ -1,33 +1,61 @@
 #include "editor.h"
 #include <QToolBar>
 #include <QIcon>
+#include <QFile>
+#include <QTextStream>
 #include <QAction>
 #include <QMenu>
 #include <QMenuBar>
 #include <QStatusBar>
-#include <QTextEdit>
 
-textEditor::textEditor(QWidget *parent)
-    : QMainWindow(parent) {
+textEditor::textEditor(QWidget *parent) : QMainWindow(parent) {
         
   QPixmap newpix("Resources/new.png");
   QPixmap openpix("Resources/open.png");
   QPixmap quitpix("Resources/quit.png");
   QPixmap savepix("Resources/save.png");
 
+  fileName="test.txt";
+  // filename="";
+  edit = new QTextEdit(this);  
+
   QToolBar *toolbar = addToolBar("main toolbar");
   toolbar->addAction(QIcon(newpix), "New File");
   toolbar->addAction(QIcon(openpix), "Open File");
-  toolbar->addAction(QIcon(savepix), "Save File");
+  QAction *save = toolbar->addAction(QIcon(savepix), "Save File");
   toolbar->addSeparator();
   toolbar->addSeparator();
-  QAction *quit2 = toolbar->addAction(QIcon(quitpix), 
-      "Quit Application");
-  connect(quit2, SIGNAL(triggered()), qApp, SLOT(quit()));
+  QAction *quit = toolbar->addAction(QIcon(quitpix), "Quit Application");
 
-  QTextEdit *edit = new QTextEdit(this);  
-
+  connect(quit, SIGNAL(triggered()), qApp, SLOT(quit()));
+  connect(save, SIGNAL(triggered()), this, SLOT(saveFile()));
+  
   setCentralWidget(edit);
 
   statusBar()->showMessage("Ready");
+}
+
+void textEditor::saveFile(){
+	QString x = edit->toPlainText();
+	// std::string s = x.toUtf8().constData();
+	// std::cout<<s<<std::endl;
+	if(fileName.isEmpty()){
+		//open a box to take file name as input
+		//if file name already exists tell user that prv. file will be overwrited 
+		//unless he changes the name
+		//save the file with the contents
+		// std::cin>>fileName;
+	}
+	else{
+		QFile file(fileName);
+		if (file.open(QIODevice::WriteOnly)){
+			QTextStream out(&file);
+			out<<x;
+		}
+		else{
+			qWarning("Could not open file");
+		}
+		file.close();
+	}
+
 }
--- editor.h
+++ editor.h
@@ -3,11 +3,20 @@
 
 #include <QMainWindow>
 #include <QApplication>
+#include <QTextEdit>
+#include <bits/stdc++.h>
 
 class textEditor : public QMainWindow {
     
   Q_OBJECT  
 
   public:
-    textEditor(QWidget *parent = 0);
+  	textEditor(QWidget *parent = 0);
+
+  private slots:
+  	void saveFile();
+
+  private:	
+  	QTextEdit* edit;
+  	QString fileName;
 };
