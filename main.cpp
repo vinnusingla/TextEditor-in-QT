851738c9ff77347b6abf74e1f3a472d4ec79f794
Vinayak Singla <singla.1@iitj.ac.in>
2018-01-10 15:13:12 +0530

    added application icon

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[]) {
 	int width = screen->width();
 	textEditor window;
 	window.setWindowTitle("Personal Editor");
+	window.setWindowIcon(QIcon("Resources/icon.png"));
 	// window.setGeometry()
 	window.resize(width, height);
 	window.show();
